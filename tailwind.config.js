41b71214e5f2e02263a6d4e9d36fae4a82058bbc
Milan-Koelen <67652887+Milan-Koelen@users.noreply.github.com>
2022-02-26 17:52:01 +0100

    Config & dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'template-purple': '#5267DF',
+        'template-red': '#FA5959',
+        'template-blue': '#242A45',
+        'template-grey': '#9194A2',
+        'template-white': '#f7f7f7',
+      }
+    },
   },
   variants: {
     extend: {},
